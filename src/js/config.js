af497b610f8ec68203e1461c012c87140138772e
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2023-05-14 03:31:43 +0530

    First commit.

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '75dd8a9e-7dbc-4919-b6f2-6f04e3d869c7';
+export const MODAL_CLOSE_SEC = 2.5;
